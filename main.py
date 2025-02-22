0e525bb26534228f89f5b99afeee8b8b8977a457
Aaryan <downloadsoft17@gmail.com>
2025-02-22 22:13:47 +0545

    Add project's description, images and links

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ st.write(description)
 
 
 # Project Section
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 # Converting the data.csv file into a well-structured table using pandas
 df  = pandas.read_csv("data.csv", sep=";")
@@ -29,7 +29,18 @@ df  = pandas.read_csv("data.csv", sep=";")
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("Images/"+row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("Images/"+row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
+
+
+
